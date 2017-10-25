b1c847787873c4eb8a3b1983e917559f4d380b64
Aaron Lun <alun@wehi.edu.au>
2017-10-25 11:34:40 +0100

    Initial commit of 10X neuron resources.

---
+++ inst/scripts/make-data.R
@@ -0,0 +1,24 @@
+# Downloading the data.
+
+path <- "1M_neurons_filtered_gene_bc_matrices_h5.h5"
+download.file("http://cf.10xgenomics.com/samples/cell-exp/1.3.0/1M_neurons/1M_neurons_filtered_gene_bc_matrices_h5.h5", path)
+
+# Loading the data into R.
+
+# devtools::install_github("mtmorgan/TENxGenomics")
+library(TENxGenomics)
+tenx.se <- tenxSummarizedExperiment(path)
+tenx.se
+
+# Converting into a `HDF5Matrix` object
+
+tenxmat <- TENxMatrix(path)
+
+library(HDF5Array)
+options(DelayedArray.block.size=1e9) # 1GB block size.
+mat.out <- writeHDF5Array(tenxmat, file="10x_neurons.h5", name="neurons", chunk_dim=c(100, 100))
+
+# Constructing a new SingleCellExperiment object for serialization.
+
+se.out <- SingleCellExperiment(list(counts=mat.out), rowData=rowData(tenx.se), colData=colData(tenx.se))
+saveRDS(se.out, file="sce.rds")                               
