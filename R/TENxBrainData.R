7024abcc3bb9905b34e5a56e6797519e774b3474
Martin Morgan <martin.morgan@roswellpark.org>
2017-10-26 18:25:19 -0400

    update TENxBrainData()
    - retrieve h5 file from ExperimentHub
    - construct SingleCellExperiment from h5, rowData, and colData

--- R/TENxBrainData.R
+++ R/TENxBrainData.R
@@ -3,15 +3,24 @@ TENxBrainData <- function()
 # and modifies the path of the HDF5 object to use the 
 # system location of the installation.
 {
-    # ... downloading the RDS file:
-    # rds.loc <- ...
+    base <- "1M_neurons_filtered_gene_bc_matrices_h5"
 
-    # ... downloading the HDF5 file:
-    # h5.loc <- ...
-    
-    sce <- readRDS(rds.loc)
-    counts <- assay(sce)
-    counts@seed@file <- h5.loc
-    assay(sce) <- counts
-    return(sce)
+    ## row and column data
+    path <- system.file(package = "TENxBrainData", "extdata")
+
+    fname <- file.path(path, paste0(base, "_rowData.rds"))
+    rowData <- readRDS(fname)
+
+    fname <- file.path(path, paste0(base, "_colData.rds"))
+    colData <- readRDS(fname)
+
+    ## HDF5, from ExperimentHub:
+    rdatapath <- paste0("TENxBrainData/", base, "_rectangular.h5")
+    h5file <- query(ExperimentHub(), rdatapath)[[1]]
+    h5file <- "~/a/1M_neurons_filtered_gene_bc_matrices_h5_rectangular.h5"
+    h5array <- HDF5Array(h5file, "counts")
+
+    SingleCellExperiment(
+        list(counts = h5array), rowData = rowData, colData = colData
+    )
 }
