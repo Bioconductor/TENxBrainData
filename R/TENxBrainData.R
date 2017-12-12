6d919aa514b310794bb49adc91af96e397073f7a
Martin Morgan <martin.morgan@roswellpark.org>
2017-12-12 09:55:11 -0500

    less chatty data load

--- R/TENxBrainData.R
+++ R/TENxBrainData.R
@@ -3,18 +3,21 @@ TENxBrainData <- function()
 # and modifies the path of the HDF5 object to use the 
 # system location of the installation.
 {
+    hub <- ExperimentHub()
     base <- "1M_neurons_filtered_gene_bc_matrices_h5"
 
     ## row and column data
     rdatapath <- paste0("TENxBrainData/", base, "_rowData.rds")
-    rowData <- query(ExperimentHub(), rdatapath)[[1]]
+    rowData <- query(hub, rdatapath)[[1]]
 
-    rdatapath <- paste0("TENxBrainData/", base, "_colData.rds")
-    colData <- query(ExperimentHub(), rdatapath)[[1]]
+    suppressMessages({
+        rdatapath <- paste0("TENxBrainData/", base, "_colData.rds")
+        colData <- query(hub, rdatapath)[[1]]
 
-    ## HDF5, from ExperimentHub:
-    rdatapath <- paste0("TENxBrainData/", base, "_rectangular.h5")
-    h5file <- query(ExperimentHub(), rdatapath)[[1]]
+        ## HDF5, from ExperimentHub:
+        rdatapath <- paste0("TENxBrainData/", base, "_rectangular.h5")
+        h5file <- query(hub, rdatapath)[[1]]
+    })
     h5array <- HDF5Array(h5file, "counts")
 
     SingleCellExperiment(
